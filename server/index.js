70bd14da5031931c3dfbb52a2fbbd4c3a4014034
Sonu-ABC <bubulbiswas318@gmail.com>
2026-05-24 19:48:45 +0530

    Initial commit: Talkroom full-stack chat application

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
+import messagesRoutes from "./routes/MessagesRoute.js";
+import setupSocket from "./socket.js";
+import channelRoutes from "./routes/ChannelRoutes.js";
+
+dotenv.config();// dotenv is used to load environment variables from a .env file into process.env.
+
+const app = express();
+const port = process.env.PORT;
+const databaseURL = process.env.DATABSE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN], // origin:[] is used to allow only the specified origin to access the server. 
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files", express.static("uploads/files"));
+
+app.use(cookieParser());// cookie-parser is a middleware for parsing cookies.
+app.use(express.json());// express.json() is a middleware for parsing JSON data.
+
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactsRoutes);
+app.use("/api/messages", messagesRoutes);
+app.use("/api/channel", channelRoutes);
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+setupSocket(server);
+
+mongoose
+  .connect(databaseURL)
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
